3b4baa1f764477007dd7c9d9bb9d25aa81b6f08e
TryciasDev <trycias.dev@gmail.com>
2018-11-28 07:37:17 +0100

    degage fichier session

--- tmp/2go7akemgge8g.1jkx3czz4418b.php
+++
@@ -1,4 +0,0 @@
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-  </body>
-  </html>
