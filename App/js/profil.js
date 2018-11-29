4e9c1529dba84dd73cfb3a8e33199a6ff85c5b0f
TryciasDev <trycias.dev@gmail.com>
2018-11-29 19:57:40 +0100

    ajout test js

--- App/js/profil.js
+++ App/js/profil.js
@@ -1,10 +1,46 @@
 $(document).ready(function(){
 	$("#Save").click(function() {
-		$("#formulaire").validate();
-		var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-		if (!testEmail.test($("#Email").val()))
+		//$("#formulaire").validate();
+		//Vérifier qu'il y a au moins un amis de sélectionner si amis il y a
+		$(//trouver selecteur sur le nom ou voir si jquery n'a pas un truc spécial checkbox 
+		if($("#Prenom").val() == undefined || $("#Prenom").val().trim() == "")
 		{
-			alert('Le format de l\'adresse mail n\'est pas valide');
+			alert('Le prénom est obligatoire');
 		}
+	});
+	$("#Nom").focusout(function(){
+		if($("#Nom").val() == undefined || $("#Nom").val().trim() == "")
+		{
+			$("#NomErreur").text('Le nom est obligatoire');
+			$("#NomErreur").show();
+		} else {
+			$("#NomErreur").text(''); 
+			$("#NomErreur").hide();
+		}
+	});
+	$("#Email").focusout(function(){
+		var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+		if($("#Email").val() == undefined || $("#Email").val().trim() == "")
+		{
+			$("#EmailErreur").text('Le mail est obligatoire');
+			$("#EmailErreur").show();
+		}else if (!testEmail.test($("#Email").val()))
+		{
+			$("#EmailErreur").text('Le format de l\'adresse mail n\'est pas valide');
+			$("#EmailErreur").show();
+		} else {
+			$("#EmailErreur").text(''); 
+			$("#EmailErreur").hide();
+		}
+	});
+	$("#Prenom").focusout(function(){
+		if($("#Prenom").val() == undefined || $("#Prenom").val().trim() == "")
+		{
+			$("#PrenomErreur").text('Le prénom est obligatoire');
+			$("#PrenomErreur").show();
+		} else {
+			$("#PrenomErreur").text(''); 
+			$("#PrenomErreur").hide();
+		}		
 	});
 });
